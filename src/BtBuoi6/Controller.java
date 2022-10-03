8f8ff98cd798c6e0e05336702e91ccbe58fca286
xuanhanhbn <101380759+xuanhanhbn@users.noreply.github.com>
2022-10-03 20:50:19 +0700

    Bt Buoi 6 - listView

--- src/BtBuoi6/Controller.java
+++ src/BtBuoi6/Controller.java
@@ -1,25 +1,56 @@
 package BtBuoi6;
 
+import Buoi4.Phone;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Controller {
     public TextField txtPhoneNumber;
     public TextField txtName;
-    public TextField listPhoneBook;
+    public ListView<PhoneNumber> listPhoneBook;
+    public Text noticeError;
 
+    private ObservableList<PhoneNumber> phoneList = FXCollections.observableArrayList();
+    ;
     public void handleAdd(){
-        String fn = txtName.getText();
-        String ph = txtPhoneNumber.getText();
-        Integer nPhone = Integer.parseInt(ph);
-        if (equals(true)) {
-            listPhoneBook.setText(fn.concat(": ").concat(ph));
-        } else {
-            listPhoneBook.setText("Error");
+        try {
+            noticeError.setVisible(false);
+            if(txtName.getText().isEmpty() || txtPhoneNumber.getText().isEmpty()) {
+                throw new Exception("Nhập đầy đủ thông tin!!");
+            }
+            updateListBook();
+//            phoneList.add(new PhoneNumber(txtName.getText(), txtPhoneNumber.getText()));
+            outputListBook();
+
+        }catch(Exception e) {
+            noticeError.setText(e.getMessage());
+            noticeError.setVisible(true);
         }
     }
 
+    public void outputListBook() {
+        listPhoneBook.setItems(phoneList);
+        listPhoneBook.refresh();
+
+    }
+    public void updateListBook(){
+        for(PhoneNumber p: phoneList){
+            if(p.getName().equals(txtName.getText())){
+                if(!p.getPhone().equals(txtPhoneNumber.getText())){
+                    p.setPhone(txtPhoneNumber.getText());
+                    return;
+                }
+                return;
+            }
+        }
+        phoneList.add(new PhoneNumber(txtName.getText(), txtPhoneNumber.getText()));
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
